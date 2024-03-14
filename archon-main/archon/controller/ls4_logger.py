9d8650ad186f21e4c8fd6631bb9e4c823ceba6cf
David L Rabinowitz <david.rabinowitz@yale.edu>
2024-03-14 11:58:23 -0400

    synchronous and sumultaneous readout/fetch

---
+++ archon-main/archon/controller/ls4_logger.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: David Rabinowitz (david.rabinowitz@yale.edu)
+# @Date: 2023-12-18
+# @Filename: ls4_device.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+# Class to handle LS4 logging
+
+import sys
+from archon import log
+import logging
+
+class LS4_Logger():
+
+    def __init__(
+      self,
+      leader: bool | None = None,
+      name=None,
+      info=None,
+      debug=None,
+      error=None,
+      warn=None,
+      critical=None
+    ):
+
+      if leader is not None:
+         print("leader = %s" % leader)
+         self.leader=leader
+      else:
+         self.leader=False
+
+      if name is not None:
+         self.name=name
+      else:
+         self.name="ctrl"
+
+      self.info = self.ls4_info
+      self.debug= self.ls4_debug
+      self.warn = self.ls4_warn
+      self.error = self.ls4_error
+      self.critical= self.ls4_critical
+
+      if info is not None:
+         self.info = info
+      if debug is not None:
+         self.debug= debug
+      if warn is not None:
+         self.warn = warn
+      if error is not None:
+         self.error = error
+      if critical is not None:
+         self.critical= critical
+
+    def ls4_info(self,str):
+      if self.leader or self.name == "ctrl1":
+        str1 = "%s: "%self.name + str
+        log.info(str1)
+
+    def ls4_error(self,str):
+        str1 = "%s: "%self.name + str
+        log.error(str1)
+        sys.stdout.flush()
+        sys.stderr.flush()
+
+    def ls4_warn(self,str):
+        str1 = "%s: "%self.name + str 
+        log.warn(str1)
+        sys.stdout.flush()
+        sys.stderr.flush()
+
+    def ls4_debug(self,str):
+      if self.leader or self.name == "ctrl1":
+        str1 = "%s: "%self.name + str
+        log.debug(str1)
+        sys.stdout.flush()
+        sys.stderr.flush()
+
+    def ls4_critical(self,str):
+        str1 = "%s: "%self.name + str
+        log.critical(str1)
+        sys.stdout.flush()
+        sys.stderr.flush()
+
+    def set_format(self,log_format: str):
+
+        try:
+             logging.basicConfig(format=log_format)
+        except Exception as e:
+             self.warn("unable to set log format to %s" % log_format)
+
+    def set_level(self,log_level: str):
+
+        log.setLevel(logging.NOTSET)
+        if log_level in ["DEBUG","debug","Debug"]:
+           log.setLevel(logging.DEBUG)
+        elif log_level in ["INFO","info","Info"]:
+           log.setLevel(logging.INFO)
+        elif log_level in ["WARN","warn","Warn"]:
+           log.setLevel(logging.WARNING)
+        elif log_level in ["ERROR","error","Error"]:
+           log.setLevel(logging.ERROR)
+        elif log_level in ["CRITICAL","critical","Critical"]:
+           log.setLevel(logging.CRITICAL)
+
+
+#"""
+if( __name__ == '__main__'):
+
+  lg=LS4_Logger(leader=True)
+  lg.set_format("[####%(filename)s:%(lineno)s:%(funcName)s### ] %(message)s")
+
+  lg.info("hello")
+#"""
---
+++ scripts/make_mosaic.csh
@@ -0,0 +1,22 @@
+#!/bin/tcsh
+#
+# make mosaic image from 64 amp output of LS4 
+#
+if ( $#argv != 2 ) then
+   echo "syntax: make_mosaic.csh [prefix (e.g. "test")] [sequence number]"
+   exit
+endif
+set prefix = $argv[1]
+set seq_num = `printf "_%03d_" $argv[2]`
+echo "prefix: $prefix seq_num: $seq_num"
+set output = `printf "mos_%03d.fits" $argv[2]`
+echo "output: $output"
+
+set l = `ls "$prefix"*"$seq_num"*".fits"`
+echo "l: $l"
+set l1 = `echo $l | sed -e "s/ /,/g"`
+echo "l1: $l1"
+python  $LS4_CONTROL_ROOT/scripts/mosaic.py  --images $l1 --output $output 
+
+
+
